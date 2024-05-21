632cb6f69bb5f4fe6acaf17ad6fbfc3ddb7d096e
leonardorafaelli <leonardorafaelli@gmail.com>
2024-05-21 16:34:03 +0200

    feat: adds paypalConfig class

---
+++ src/main/java/com/rafaelli/payment/config/PaypalConfig.java
@@ -0,0 +1,10 @@
+package com.rafaelli.payment.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PaypalConfig {
+    private String clientId;
+    private String clientSecret;
+    private String mode;
+}
