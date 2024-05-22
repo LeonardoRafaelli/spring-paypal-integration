61d29bf9b4be7019806d428acbffbb5ceb401792
leonardorafaelli <leonardorafaelli@gmail.com>
2024-05-22 17:13:54 +0200

    feat: adds @Value annotation to PaypalConfig atts

--- src/main/java/com/rafaelli/payment/config/PaypalConfig.java
+++ src/main/java/com/rafaelli/payment/config/PaypalConfig.java
@@ -1,10 +1,15 @@
 package com.rafaelli.payment.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PaypalConfig {
+    // Gets the value from application.properties
+    @Value("${paypal.client-id}")
     private String clientId;
+    @Value("${paypal.client-secret}")
     private String clientSecret;
+    @Value("${paypal.mode}")
     private String mode;
 }
