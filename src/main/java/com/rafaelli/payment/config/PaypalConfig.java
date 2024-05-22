25e319f058a799a5435ca040817ea46a4c775b9f
leonardorafaelli <leonardorafaelli@gmail.com>
2024-05-22 17:18:23 +0200

    adds app properties in yml file

--- src/main/java/com/rafaelli/payment/config/PaypalConfig.java
+++ src/main/java/com/rafaelli/payment/config/PaypalConfig.java
@@ -1,6 +1,8 @@
 package com.rafaelli.payment.config;
 
+import com.paypal.base.rest.APIContext;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -12,4 +14,9 @@ public class PaypalConfig {
     private String clientSecret;
     @Value("${paypal.mode}")
     private String mode;
+
+    @Bean
+    public APIContext apiContext() {
+        return new APIContext(clientId, clientSecret, mode);
+    }
 }
